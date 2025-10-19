5526a7441dba6d05eb2776a4c883b7ded417fb5b
Oswaldo_hub <adriaanrojas23@gmail.com>
2025-10-19 14:36:40 -0500

    Uploading Spring Boot autoconfiguration with dependencies, using Spring Initialization

---
+++ src/test/java/Spring/ProyectLearning/ProyectLearning/ProyectLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.ProyectLearning.ProyectLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
